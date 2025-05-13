3a797d3d087ba33920a5d389b396d5d5fd8d0907
TheDinoKingK <Kaveencode365@gmail.com>
2025-05-12 22:04:02 -0400

    wip section 4, ran into ts error

---
+++ dot-net/reactivities/client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,29 @@
+import { Grid } from "@mui/material";
+import ActivityList from "./ActivityList";
+import ActivityDetail from "../details/ActivityDetail";
+
+type Props = {
+  activities: Activity[]
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  selectedActivity?: Activity;
+}
+
+export default function ActivityDashboard({ activities, cancelSelectActivity, selectActivity, selectedActivity }: Props) {
+  return (
+    <Grid container spacing={3}>
+      <Grid size={7}>
+        <ActivityList 
+          activities={activities}
+          selectActivity={selectActivity}
+        />
+      </Grid>
+      <Grid size={5}>
+        {activities[0] && <ActivityDetail 
+          activity={selectedActivity}
+          cancelSelectedActivity={cancelSelectActivity}
+        />}
+      </Grid>
+    </Grid>
+  )
+}
