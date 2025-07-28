1a8cf4f93e7880db7411ab8eaa393473db7999bb
TheDinoKingK <Kaveencode365@gmail.com>
2025-07-28 19:08:55 -0400

    added a funny loading bar & a counter page using mobx, sect 9 done

--- dot-net/reactivities/README.md
+++ dot-net/reactivities/README.md
@@ -3,6 +3,10 @@
 A fairly complex web app made with dotNET and React I started in late feb, 2025  
 this was also the first time I'm developing in the apple mac ecosystem (notes on that coming soon)  
 
+## Bonus tasks after completion:
+- add a dark mode theme & toggle (automatically chooses based off system preference if possible)
+- better mobile compatibility
+
 ## Running App
 **note: this was created with a slightly custom vs code instance and might require different setup with other IDEs**
 
--- dot-net/reactivities/client/src/lib/hooks/useActivities.ts
+++ dot-net/reactivities/client/src/lib/hooks/useActivities.ts
@@ -1,14 +1,19 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
+import { useLocation } from "react-router";
 
 export function useActivities(id?: string) {
   const queryClient = useQueryClient();
+  const location = useLocation();
+
   const { data: activities, isPending } = useQuery({
     queryKey: ['activities'],
     queryFn: async () => {
       const response = await agent.get<Activity[]>('/activities');
       return response.data;
-    }
+    },
+    // this funny little line makes sure we only start trying to re-fetch the activities when we're loading the activities page
+    enabled: !id && location.pathname === '/activities'
   });
 
   const {data: activity, isLoading: isLoadingActivity} = useQuery({
